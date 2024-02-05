9f7b06a258bbe40eea6f348e74752b45cb22e18a
shashank-pinelabs <shashank.varshney@pinelabs.com>
2024-02-05 12:49:19 +0530

    First hogwarts test

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber());
+    },
+    specPattern: 'cypress/e2e/*.feature',
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
+  },
+});
